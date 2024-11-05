f25a518ea6b518eee98db4364bf74b48c32d0eaf
deltechdesigns <deltechdesigns@gmail.com>
2024-11-05 07:37:22 +0100

    remove /api from the api  url in render link in  api file

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://fci-communication-platform-api.onrender.com/api",
+  baseURL: "https://fci-communication-platform-api.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
