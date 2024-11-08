7398f17d71e9f75422dbc1d5633bf8fc7035fcab
deltechdesigns <deltechdesigns@gmail.com>
2024-11-08 11:56:41 +0100

    correct the url

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://fci-communication-platform.vercel.app/",
+      "https://fci-communication-platform.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
